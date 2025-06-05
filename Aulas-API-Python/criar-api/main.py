708f68917e6f819696571dde4d640f20f0860fea
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-06-05 12:36:51 -0300

    Add comments to help students

--- Aulas-API-Python/criar-api/main.py
+++ Aulas-API-Python/criar-api/main.py
@@ -5,9 +5,24 @@ from request.model.ProductModel import Product
 
 app = FastAPI()
 
+# Aqui estamos delcarando uma rota da nossa API que:
+# - Tem como url '/products?category=something&send=sometghing'
+# - Necessita do método GET para usá-lo
+app.get('/products')
+def searchByCategory(response:Response, category:str, send:str):
+    # Essa é a função que vai lidar com a chamada da rota. Nessa função, nós:
+    # - Filtramos os produtos pelos query parameters passados
+   
+    # - Caso não tenha nenhum produto com os filtros, retornamos uma resposta específica:
+    #   - Um status code: 404 not found, indicando que neenhum recurso foi encontrado
+    #   - Um corpo de resposta (JSON) com uma mensagem - Será um padrão
+    #     enviar esse informação em todas as repostas
+   
+    # - Caso haja produtos, retornaremos:
+    #   - Um status code 200 OK
+    #   - A mensagem, como padrão
+    #   - Os produtos encontrados
 
-app.get('/products/category/')
-def searchByCategory(response:Response, category:str = None, send:str = None):
     products_filtered:list[dict] = []
     
     for product in DB.values():
@@ -30,6 +45,13 @@ def searchByCategory(response:Response, category:str = None, send:str = None):
 
 app.get('/products/{product_id}')
 def searchById(product_id:int, response:Response):
+    # Nessa rota, é possível através do id do produto
+    # localizá-lo no nosso banco de dados, sendo o id pasado direto na url.
+
+    # Desse modo, será retornado o único item por completo, caso encontrado.
+
+    # Caso o id não exista, um not found será retornado.
+
     try:
         product = DB[product_id]
     except KeyError:
@@ -45,8 +67,12 @@ def searchById(product_id:int, response:Response):
 
 app.post('/products/')
 def addProduct(product: Product, response:Response):
-    
-    DB.append(product.model_dump())
+    # nessa rota, precisamos de outro método HTTP, o POST,
+    # já que estaremos enviando algo para a API, pelo corpo
+    # da requisição (JSON)
+
+    id = len(DB) + 1
+    DB.update({id: product.model_dump()})
     return {'mesasge': 'product added sucessfully', 'product': product}
 
 
@@ -55,6 +81,8 @@ def addProduct(product: Product, response:Response):
 
 app.put('/products/{product_id}')
 def updateProduct(product_id:int, response:Response, product_req: Product):
+    # Para atualizar recursos, usamos outro método, o PUT ou PATCH
+    
     try:
         product = DB[product_id]
     except KeyError:
