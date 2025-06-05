a0ecbe528d8d938f9c7abfc73872b4993f3196d2
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-06-04 21:17:58 -0300

    Split into consume and create api studies

---
+++ Aulas-API-Python/consumir-api/main.py
@@ -0,0 +1,22 @@
+import requests
+
+
+# Primeiro: Salvar o caminho para de onde requistaremos os dados
+url = ""
+
+# Segundo: Chamar a biblioteca, juntamente ao método http que desejamos utilizar,
+# Passando os parâmetros necessários:
+# - url + query parâmetros (se necessários)
+# - headers (se necesário)
+# - cookies (se necessaŕio)
+
+# Não esqueça que salvar o retorno em uma váriavel. Ele retornará um objeto que podemos manipular
+
+response = requests.get(url)
+
+# Caso precise ver o status code retornado:
+print(response.status_code)
+
+# Para coletar o corpo da resposta em forma de dicionário python:
+json = response.json()
+
