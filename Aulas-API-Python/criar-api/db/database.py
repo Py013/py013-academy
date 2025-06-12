01c1fa8014d4daee93c0071e141eb62913dd3c41
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-06-12 10:04:49 -0300

    change to string ids

--- Aulas-API-Python/criar-api/db/database.py
+++ Aulas-API-Python/criar-api/db/database.py
@@ -1,38 +1,35 @@
-DB: dict[int, dict] = {
-    1: {
+DB: dict[str, dict] = {
+    '1': {
+        'id': '1',
         'description': 'Notebook Dell Inspiron 15 3000',
         'category': 'Computador',
         'quantity': 450,
         'send_type': 'Nacional',
         'price': 50.70,
     },
-    2: {
+   '2': {
+        'id': '2',
         'description': 'Placa de vídeo NVIDIA 5090',
         'category': 'Componente eletrônico',
         'quantity': 41,
         'send_type': 'Internacional',
         'price': 68.12,
     },
-    3: {
+    '3': {
+        'id': '3',
         'description': 'Cadeira gamer com suspensão a ar',
         'category': 'Mobília',
         'quantity': 220,
         'send_type': 'Nacional',
         'price': 335.53,
     },
-    4: {
+    '4': {
+        'id': '4',
         'description': 'Mouse profissional para designers 3D',
         'category': 'Periferico',
         'quantity': 21,
         'send_type': 'Internacional',
         'price': 225.50,
-    },
-    5: {
-        'description': 'Fone de ouvido Kuba Uni',
-        'category': 'Periferico',
-        'quantity': 21,
-        'send_type': 'Nacional',
-        'price': 0.15,
     },
 }
 
