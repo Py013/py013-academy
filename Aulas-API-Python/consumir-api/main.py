706b7fe2a475d01eafc64b25575a6d9061163e33
Miguel-Andrade-Cruz <angelium.eddra1972@gmail.com>
2025-06-12 10:02:39 -0300

    add 'if name: main', query parameters and erquest body, internal error handling

--- Aulas-API-Python/consumir-api/main.py
+++ Aulas-API-Python/consumir-api/main.py
@@ -1,23 +1,34 @@
 import requests
 
+def main():
+    # Primeiro: Salvar o caminho para de onde requistaremos os dados
+    url = "http://127.0.0.1:8000/products"
 
-# Primeiro: Salvar o caminho para de onde requistaremos os dados
-url = "http://127.0.0.1:8000/products?category=Periferico&send=Internacional"
+    # Segundo: Chamar a biblioteca, juntamente ao método http que desejamos utilizar,
+    # Passando os parâmetros necessários:
+    # - url + query parâmetros (se necessários)
+    # - corpo (body) da requisição
+    # - headers (se necesário)
+    # - cookies (se necessaŕio)
 
-# Segundo: Chamar a biblioteca, juntamente ao método http que desejamos utilizar,
-# Passando os parâmetros necessários:
-# - url + query parâmetros (se necessários)
-# - headers (se necesário)
-# - cookies (se necessaŕio)
+    query = '?category=Perfiferico&send=Internacional'
 
-# Não esqueça que salvar o retorno em uma váriavel. Ele retornará um objeto que podemos manipular
+    request_body = {
+        'price': 1458.28,
+    }
 
-response = requests.get(url)
+    # Não esqueça que salvar o retorno em uma váriavel. Ele retornará um objeto que podemos manipular
+    response = requests.get(url + '/70', json=request_body)
 
-# Caso precise ver o status code retornado:
-print(response.status_code)
+    # Caso precise ver o status code retornado:
+    if response.status_code == 500:
+        print("internal error")
+        return
+    # Para coletar o corpo da resposta em forma de dicionário python:
+    json = response.json()
 
-# Para coletar o corpo da resposta em forma de dicionário python:
-json = response.json()
+    print(json)
 
-print(json)
+
+if __name__ == '__main__':
+    main()
